5fd77ef0e11cebe33c15dcdcdc9f63fbb64126e0
Rahim <kristopherab@gmail.com>
2025-08-05 20:12:52 +0100

    update category type

--- internal/category/category.go
+++ internal/category/category.go
@@ -1,14 +1,6 @@
 package category
 
 type Category struct {
-	CategoryID   int    `json:"ID"`
-	CategoryName string `json:"category"`
-}
-
-type Repository interface {
-	GetCategoryByName(n string) (*Category, error)
-	GetCategoryByID(id int) (*Category, error)
-	AddNewCategory(n string) (int64, error)
-	UpdateCategory(n string, id int) (int64, error)
-	DeleteCategory(id int) (int64, error)
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
